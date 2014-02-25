043c40c6d6710e3137dcd18cf78bf9cf8323389f
Ivo Nascimento <iannsp@gmail.com>
2014-02-25 01:20:25 -0300

    command line implementation

--- rulez.js
+++ rulez.js
@@ -1,22 +1,21 @@
-/*mode = 0; // {0:commandline,1:daemon}
-process.argv.forEach(function (val, index, array) {
-  console.log(index + ': ' + val);
-});
-*/
+var commandline = require("./lib/commandline.js");
+var rulezEngine = require("./lib/rulezengine.js");
 
-data = {"sexo":"masculino","idade":35,"ecliente":true};
-//console.log(data.sexo);
-rule = ['data.sexo =="masculino"', function(data){
-    return data.idade -10;
-}];
-result = [];
-for(i=0; i< rule.length; i++){
-    console.log(rule[i]);
-    if(typeof(rule[i])=='function'){
-        console.log ( 
-            (rule[i])( data )
-        );
-    }else{
-        console.log( eval(rule[i]) );
-    }
+cmd = new commandline(process.argv);
+cmd = cmd.process();
+
+
+if (cmd && cmd.mode == "commandline"){
+    if (cmd.ruleset ==null)
+        process.stdout.write("Wrong Parameters. Missing Ruleset -r parameter.\n");
+    if (cmd.fact ==null)
+        process.stdout.write("Wrong Parameters. Missing Fact -f parameter.\n");
+
+        bre = new rulezEngine();
+        if (!bre.setRuleSet(cmd.ruleset)){
+            process.stdout.write("RuleSet "+cmd.ruleset+" does not exist.\n");
+        };
+        
+        console.log(bre.run(cmd.fact));
+//    process.stdout.write( bre.run(cmd.fact));
 }
