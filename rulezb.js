86e2bb34d6438fe1c37b1b1ffb06fcaa7a6fc8c6
Ivo Nascimento <iannsp@gmail.com>
2014-03-02 02:48:00 -0300

    add support for daemon mode

--- rulezb.js
+++ rulezb.js
@@ -4,18 +4,48 @@ var rulezEngine = require("./lib/rulezengine.js");
 cmd = new commandline(process.argv);
 cmd = cmd.process();
 
+bre = new rulezEngine();
+
+if (cmd.ruleset ==null)
+    process.stdout.write("Wrong Parameters. Missing Ruleset -r parameter.\n");
+    if (!bre.setRuleSet(cmd.ruleset)){
+        process.stdout.write("RuleSet "+cmd.ruleset+" does not exist.\n");
+    };
 
 if (cmd && cmd.mode == "commandline"){
-    if (cmd.ruleset ==null)
-        process.stdout.write("Wrong Parameters. Missing Ruleset -r parameter.\n");
     if (cmd.fact ==null)
         process.stdout.write("Wrong Parameters. Missing Fact -f parameter.\n");
 
-        bre = new rulezEngine();
-        if (!bre.setRuleSet(cmd.ruleset)){
-            process.stdout.write("RuleSet "+cmd.ruleset+" does not exist.\n");
-        };
     var results = bre.run(cmd.fact);
     console.log(bre.respond(results));
+    return;
 //    process.stdout.write( bre.run(cmd.fact));
 }
+
+http = require('http');
+fs = require('fs');
+server = http.createServer( function(req, res) {
+    if (req.method == 'POST') {
+        req.on('data', function (data) {
+                    bre.postDATA = data.toString();
+        });
+        req.on('end', function () {
+            bre.POSTRESULT = bre.run(JSON.parse(bre.postDATA));
+            res.writeHead(200, {'Content-Type': 'application/json'});
+            respond = bre.respond(bre.POSTRESULT);
+            res.write(JSON.stringify(respond));
+            res.end();
+        });
+    }
+    else
+    {
+        var html = fs.readFileSync('./index.html');
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.end(html);
+    }
+});
+
+host = "10.0.0.100";
+server.listen(cmd.port, host);
+server.bre = bre;
+console.log('RulesB Running at http://' + host + ':' + cmd.port);
