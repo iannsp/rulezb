303c8d2dbd5ea9077a8e64eaf6e99b6e65a99d76
Ivo Nascimento <iannsp@gmail.com>
2014-03-02 00:51:49 -0300

    declare var

--- rulezb.js
+++ rulezb.js
@@ -15,7 +15,7 @@ if (cmd && cmd.mode == "commandline"){
         if (!bre.setRuleSet(cmd.ruleset)){
             process.stdout.write("RuleSet "+cmd.ruleset+" does not exist.\n");
         };
-    results = bre.run(cmd.fact);
+    var results = bre.run(cmd.fact);
     console.log(bre.respond(results));
 //    process.stdout.write( bre.run(cmd.fact));
 }
